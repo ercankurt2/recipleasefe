521b1e80b4930c0061da4df3e979ab9259e72d78
MeteHan98 <s0584169@htw-berlin.de>
2024-01-07 23:04:43 +0100

    Kommentar für jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1'  // Dieser Ausdruck fängt Importe ab, die mit @/ beginnen, ersetzt '@/'-Pfade durch das entsprechende 'src'-Verzeichnis
   },
   transformIgnorePatterns: ['/node_modules/(?!(axios)/)']
 };
