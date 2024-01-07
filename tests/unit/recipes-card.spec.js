b714f6c98de5a4d71cad3591711366cfbb9f5a98
MeteHan98 <s0584169@htw-berlin.de>
2024-01-07 23:18:16 +0100

    Bugfix Test für RecipesCard.vue

--- tests/unit/recipes-card.spec.js
+++ tests/unit/recipes-card.spec.js
@@ -1,3 +1,4 @@
+import { mount } from '@vue/test-utils'
 import RecipesCard from "@/components/RecipesCard.vue";
 
 describe('Testing RecipesCard.vue', () => {
@@ -7,15 +8,18 @@ describe('Testing RecipesCard.vue', () => {
       id: 1,
       title: "Kartoffelsalat",
       beschreibung: "Lorem ipsum"
-    }
+    };
     const wrapper = mount(RecipesCard,{
-      propsData:{
+      propsData: {
         recipe: testRecipe
       }
-      })
+      });
 
     //then
     const cardTitle = wrapper.find('.card-title')
-    expect(cardTitle.text()).toBe(`${testRecipe.title} ${testRecipe.beschreibung}`)
+    const cardText = wrapper.find('.card-text');
+
+    expect(cardTitle.text()).toBe(testRecipe.title);
+    expect(cardText.text()).toBe(testRecipe.beschreibung);
   });
 })
