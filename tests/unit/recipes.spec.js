f30f2860540bcf4d8209fb033e787c45b5661e8a
MeteHan98 <s0584169@htw-berlin.de>
2024-01-07 18:24:31 +0100

    zweiter Test für RecipesView.vue

--- tests/unit/recipes.spec.js
+++ tests/unit/recipes.spec.js
@@ -1,5 +1,6 @@
 import RecipesView from "@/views/RecipesView.vue";
 import RecipesCardList from "@/components/RecipesCardList.vue";
+import RecipesCreateForm from "@/components/RecipesCreateForm.vue";
 
 describe('Testing RecipesView.vue', ()=>{
   it('should have recipes card list component',() =>{
@@ -10,5 +11,14 @@ describe('Testing RecipesView.vue', ()=>{
     const cardList = wrapper.findComponent(RecipesCardList)
     expect(cardList.exists()).toBeTruthy()
   })
+
+  it('should have recipes create form component', () => {
+    //when
+    const wrapper = mount(RecipesView)
+
+    //then
+    const createForm = wrapper.findComponent(RecipesCreateForm)
+    expect(createForm.exists()).toBeTruthy()
+  });
 
 })
