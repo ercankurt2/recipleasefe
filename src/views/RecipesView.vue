4ec36fa46e0035d13a2d0f589ffabc0378572363
MeteHan98 <s0584169@htw-berlin.de>
2024-01-08 19:20:44 +0100

    RecipesView.vue

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
-    <h1>Rezepte</h1>
-    <div class="container-fluid">
-      <recipes-card-list :recipes="recipes"></recipes-card-list>
-    </div>
-    <recipes-create-form @created="addRecipe"></recipes-create-form>
+  <h1>Rezepte</h1>
+  <div class="container-fluid">
+    <recipes-card-list :recipes="this.recipes"></recipes-card-list>
   </div>
+  <recipes-create-form @created="addRecipe"></recipes-create-form>
 </template>
 
-<script>
+<script type ='module'>
+import axios from 'axios'
 import RecipesCardList from '@/components/RecipesCardList.vue'
 import RecipesCreateForm from '@/components/RecipesCreateForm.vue'
 
@@ -18,48 +17,40 @@ export default {
     RecipesCardList,
     RecipesCreateForm
   },
-  data () {
+  data() {
     return {
-      // Initialisierte Rezepte, die im Frontend gehalten werden.
-      recipes: [
-        {
-          id: 1,
-          title: 'Kartoffelsalat',
-          beschreibung: 'Leckerer Kartoffelsalat mit Mayonnaise und Gurken.',
-          rezeptID: 1
-        },
-        {
-          id: 2,
-          title: 'Spaghetti Carbonara',
-          beschreibung: 'Klassische italienische Pasta mit Speck und Ei.',
-          rezeptID: 2
-        }
-      ]
+      recipes: []
     }
   },
-  methods: {
-    addRecipe (newRecipe) {
-      // Fügt ein neues Rezept zum 'recipes'-Array hinzu, ohne Serverkommunikation
-      this.recipes.push(newRecipe)
-    },
 
-    getPicture (recipe) {
-      if (recipe.rezeptID === 1) {
-        return require('../assets/kartoffelsalat.png')
-      } else if (recipe.rezeptID === 2) {
-        return require('../assets/test.png')
+  methods: {
+    addRecipe(recipeLocation) {
+      axios.get(`${process.env.VUE_APP_BACKEND_BASE_URL}${recipeLocation}`)
+        .then(response => {
+          this.recipes.push(response.data)
+        })
+        .catch(error => {
+          console.error('Es gab einen Fehler!', error)
+        })
+    },
+      getPicture(recipe) {
+        if (recipe.rezeptID === 1) {
+          return require('../assets/kartoffelsalat.png')
+        } else if (recipe.rezeptID === 2) {
+          return require('../assets/test.png')
+        }
       }
+    },
+    mounted() {
+      axios.get(`${process.env.VUE_APP_BACKEND_BASE_URL}/api/rezept`)
+        .then(response => {
+          console.log(response.data)
+          this.recipes = response.data
+        })
+        .catch(error => {
+          console.error('Es gab einen Fehler!', error)
+        })
     }
-  }
 }
 </script>
-<style scoped>
-.container-fluid {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh; /* Stellt sicher, dass der Container die volle Bildschirmhöhe einnimmt */
-}
 
-</style>
