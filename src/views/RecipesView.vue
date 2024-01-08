f737214801c5b764b19e0c293072e9d42e47545a
MeteHan98 <s0584169@htw-berlin.de>
2024-01-08 20:10:21 +0100

    Bugfix

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -6,7 +6,7 @@
   <recipes-create-form @created="addRecipe"></recipes-create-form>
 </template>
 
-<script type ='module'>
+<script>
 import axios from 'axios'
 import RecipesCardList from '@/components/RecipesCardList.vue'
 import RecipesCreateForm from '@/components/RecipesCreateForm.vue'
@@ -17,14 +17,13 @@ export default {
     RecipesCardList,
     RecipesCreateForm
   },
-  data() {
+  data () {
     return {
       recipes: []
     }
   },
-
   methods: {
-    addRecipe(recipeLocation) {
+    addRecipe (recipeLocation) {
       axios.get(`${process.env.VUE_APP_BACKEND_BASE_URL}${recipeLocation}`)
         .then(response => {
           this.recipes.push(response.data)
@@ -33,24 +32,23 @@ export default {
           console.error('Es gab einen Fehler!', error)
         })
     },
-      getPicture(recipe) {
-        if (recipe.rezeptID === 1) {
-          return require('../assets/kartoffelsalat.png')
-        } else if (recipe.rezeptID === 2) {
-          return require('../assets/test.png')
-        }
+    getPicture (recipe) {
+      if (recipe.rezeptID === 1) {
+        return require('../assets/kartoffelsalat.png')
+      } else if (recipe.rezeptID === 2) {
+        return require('../assets/test.png')
       }
-    },
-    mounted() {
-      axios.get(`${process.env.VUE_APP_BACKEND_BASE_URL}/api/rezept`)
-        .then(response => {
-          console.log(response.data)
-          this.recipes = response.data
-        })
-        .catch(error => {
-          console.error('Es gab einen Fehler!', error)
-        })
     }
+  },
+  mounted () {
+    axios.get(`${process.env.VUE_APP_BACKEND_BASE_URL}/api/rezept`)
+      .then(response => {
+        console.log(response.data)
+        this.recipes = response.data
+      })
+      .catch(error => {
+        console.error('Es gab einen Fehler!', error)
+      })
+  }
 }
 </script>
-
